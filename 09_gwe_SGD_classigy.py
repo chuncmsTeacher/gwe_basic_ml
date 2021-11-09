c7707ae40fa8872b7bd4ebc2740e2a57415efab7
chuncmsTeacher <487infomation@on.gwedu.go.kr>
2021-11-09 13:30:29 +0900

    'sgd'

--- 09_gwe_SGD_classigy.py
+++ 09_gwe_SGD_classigy.py
@@ -1,6 +1,6 @@
 from gwe_fishdata import FishData
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import SGDClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 
@@ -8,7 +8,8 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 
 fdata = FishData()
-bsdata = fdata.getSpecies('Bream', 'Smelt', 'Pike', 'Whitefish')
+print(fdata.species)
+bsdata = fdata.getSpecies('Bream', 'Parkki', 'Perch', 'Pike', 'Roach', 'Smelt', 'Whitefish')
 print(fdata.features)
 bsresult, bsdata = fdata.getFeatures(bsdata, 'Weight', 'Length1', 'Length2', 'Length3', 'Height', 'Width')
 
@@ -20,6 +21,21 @@ scaled = ss.transform(bsdata)
 
 train, test, target, testtarget = train_test_split(scaled, bsresult)
 
+
+sc = SGDClassifier(loss='log', max_iter=100, tol=None)
+
+trainscore = []
+testscore = []
+for i in range(300):
+    sc.partial_fit(train, target, classes=fdata.species)
+    trainscore.append(sc.score(train, target))
+    testscore.append(sc.score(test, testtarget))
+
+plt.plot(trainscore)
+plt.plot(testscore)
+plt.show()
+
+'''
 lgr = LogisticRegression()
 lgr.fit(train, target)
 print(lgr.coef_, lgr.intercept_)
@@ -29,3 +45,4 @@ print(lgr.score(test, testtarget))
 print(lgr.classes_)
 print(lgr.predict(test[:2]))
 print(lgr.predict_proba(test[:2]).round(3))
+'''
